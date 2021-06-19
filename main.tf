29816ca8bdaf6c3e49eb642f6b52443d9bb33455
gvanderberg <gvanderberg@titansoftware.co.za>
2021-06-19 17:49:08 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+data "azurerm_storage_account" "this" {
+  count = var.storage_account_create ? 0 : 1
+
+  name = var.storage_account_name
+}
+
+resource "azurerm_storage_account" "this" {
+  count = var.storage_account_create ? 1 : 0
+
+  name                      = var.storage_account_name
+  resource_group_name       = var.resource_group_name
+  location                  = var.location
+  account_kind              = var.account_kind
+  account_tier              = var.account_tier
+  account_replication_type  = var.account_replication_type
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "account_kind" {
+  description = "Defines the Kind of account."
+  type        = string
+}
+
+variable "account_tier" {
+  description = "Defines the Tier to use for this storage account."
+  type        = string
+}
+
+variable "account_replication_type" {
+  description = "Defines the type of replication to use for this storage account."
+  type        = string
+}
+
+variable "location" {
+  description = "Specifies the supported Azure location where the resource exists."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the storage account."
+  type        = string
+}
+
+variable "storage_account_create" {
+  description = "Should the storage account be created."
+  type        = bool
+}
+
+variable "storage_account_name" {
+  description = "Specifies the name of the storage account."
+  type        = string
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource."
+  type        = map(any)
+}
