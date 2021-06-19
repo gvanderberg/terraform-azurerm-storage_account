b9bfdc394def493f21a6c1132501037688dd06b8
gvanderberg <gvanderberg@titansoftware.co.za>
2021-06-19 18:23:47 +0200

    fix data lookup

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 data "azurerm_storage_account" "this" {
   count = var.storage_account_create ? 0 : 1
 
-  name = var.storage_account_name
+  name                = var.storage_account_name
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_storage_account" "this" {
